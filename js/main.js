af6e542637448994d4f723f9e5653fe7fffea65f
lijie <lijie@hortorgames.com>
2014-03-02 18:55:55 +0800

    add statusbar

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ var Task = function(data) {
     this.completed = ko.observable(false);
     if (data["due_at"]) {
         this.due = calcDateDistance(new Date(data["due_at"]));
-        console.log(this.due);
     }
 }
 
@@ -23,7 +22,11 @@ function updateTask(task, attr, status, token) {
         "success": function(res) {
             if (res.success) {
                 task[attr](!status);
+                showStatusText(true, "edit task successfully");
             }
+        },
+        error: function() {
+            showStatusText(false, "edit task failed");
         }
     });
 }
@@ -37,10 +40,27 @@ function loadRemoteImage() {
     RAL.Queue.start();
 }
 
+function showStatusText(status, text) {
+    var className = status ? "alert-success" : "alert-danger";
+    $("#statusbar").removeClass("alert-success alert-danger");
+    $("#statusbar").stop(false, true).addClass(className).text(text).fadeIn().delay(2500).fadeOut();
+}
+
 chrome.storage.sync.get("user", function(data) {
     var user = data.user, token = user["access_token"];
     function refreshTasks(callback) {
-        $.get("https://tower.im/api/v2/members/7c93bc8f591045f3b60eb8c9b073d9da/todos", {"token":token}, callback);
+        $.ajax(
+            "https://tower.im/api/v2/members/7c93bc8f591045f3b60eb8c9b073d9da/todos", {
+                data: { "token": token },
+                success: function(data) {
+                    callback(data);
+                    showStatusText(true, "refresh task list successfully.");
+                },
+                error: function() {
+                    showStatusText(false, "refresh task list failed.");
+                }
+            }
+        );
     }
 
     refreshTasks(function(data) {
@@ -71,7 +91,7 @@ chrome.storage.sync.get("user", function(data) {
                     _.each(data, function(task) {
                         tasks.push(new Task(task));
                     });
-                    loadRemoteImage()
+                    loadRemoteImage();
                 });
             }
         };
